3d9a781ae9001bfd907135a2aee021d1cd407016
liqilonglql <1455901450@qq.com>
2023-09-12 09:50:34 +0800

    第1次提交

--- demo/src/main/java/com/example/demo/test/Dmo.java
+++ demo/src/main/java/com/example/demo/test/Dmo.java
@@ -1,8 +1,13 @@
 package com.example.demo.test;
 
+import com.example.demo.bean.User;
+
 public class Dmo {
     public static void main(String[] args) {
-
+        User user = new User();
+        user.setAge(18);
+        user.setName("wangwu");
+        user.setSex("nan");
     }
 
 }
