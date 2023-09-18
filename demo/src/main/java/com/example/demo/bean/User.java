7b897a50d09840d3e5ad5d55e6fb1a04e86eb38e
liqilonglql <1455901450@qq.com>
2023-09-18 14:58:06 +0800

    测试

--- demo/src/main/java/com/example/demo/bean/User.java
+++ demo/src/main/java/com/example/demo/bean/User.java
@@ -1,10 +1,32 @@
 package com.example.demo.bean;
 
-import lombok.Data;
 
-@Data
 public class User {
     private String name;
     private  Integer   age;
     private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
